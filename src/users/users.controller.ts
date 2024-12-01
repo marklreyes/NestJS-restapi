986c3450116e9ccca65d2b73245a2d38dfaf1991
Mark Reyes <mr@marklreyes.com>
2024-12-01 15:09:30 -0800

    Add CRUD operations to Users Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    // This action returns all users
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    // This action returns a #${id} user
+    return { id };
+  }
+  // be mindful of defining a route after the findOne route as it will be treated as a param
+  // e.g. @Get(':id') will be treated as a param if defined after @Get('profile')
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    // This action returns a #${id} user
+    return { id };
+  }
+}
